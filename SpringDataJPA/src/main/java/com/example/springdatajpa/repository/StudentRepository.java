a86ebeea842d1f86c23bc24e9761b24de94b4afe
NguyenThanhHoai15 <thanhhoai150420@gmail.com>
2021-09-15 09:08:11 +0700

    push code of repo branch

--- SpringDataJPA/src/main/java/com/example/springdatajpa/repository/StudentRepository.java
+++ SpringDataJPA/src/main/java/com/example/springdatajpa/repository/StudentRepository.java
@@ -1,7 +1,8 @@
 package com.example.springdatajpa.repository;
 
 import com.example.springdatajpa.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface StudentRepository extends CrudRepository<Student, Long> {
+public interface StudentRepository extends JpaRepository<Student, Long> {
 }
