8c564edadb0ec0800175a9ca629a584d9df513f4
NguyenThanhHoai15 <thanhhoai150420@gmail.com>
2021-09-15 09:23:24 +0700

    push code of repo branch

--- SpringDataJPA/src/test/java/com/example/springdatajpa/SpringDataJpaApplicationTests.java
+++ SpringDataJPA/src/test/java/com/example/springdatajpa/SpringDataJpaApplicationTests.java
@@ -1,13 +1,38 @@
 package com.example.springdatajpa;
 
+import com.example.springdatajpa.entity.Student;
+import com.example.springdatajpa.repository.StudentRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringDataJpaApplicationTests {
 
+    @Autowired
+    private StudentRepository studentRepository
+
     @Test
-    void contextLoads() {
+    void testSaveOnRepoBranch() {
+        Student student = new Student("Nguyen Thanh", "Trung");
+        studentRepository.save(student);
+    }
+
+    @Test
+    void testReadOnRepoBranch(){
+        System.out.println(studentRepository.findAll());
+    }
+
+    @Test
+    void testUpdateRepoBranch(){
+        Student student = studentRepository.findAll().get(0);
+        student.setLastName("Hoai");
+        studentRepository.save(student);
+    }
+
+    @Test
+    void testDeleteRepoBranch(){
+        studentRepository.deleteAll();
     }
 
 }
