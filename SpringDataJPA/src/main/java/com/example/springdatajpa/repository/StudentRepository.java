80ef8ac28f0fa540e6ec0109c8490de56bc21aeb
NguyenThanhHoai15 <thanhhoai150420@gmail.com>
2021-09-15 09:41:14 +0700

    push code of JPQL branch

--- SpringDataJPA/src/main/java/com/example/springdatajpa/repository/StudentRepository.java
+++ SpringDataJPA/src/main/java/com/example/springdatajpa/repository/StudentRepository.java
@@ -2,7 +2,12 @@ package com.example.springdatajpa.repository;
 
 import com.example.springdatajpa.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    @Query("from student")
+    List<Student> selectByJPQL();
 }
--- SpringDataJPA/src/test/java/com/example/springdatajpa/SpringDataJpaApplicationTests.java
+++ SpringDataJPA/src/test/java/com/example/springdatajpa/SpringDataJpaApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.springdatajpa;
 
+import com.example.springdatajpa.repository.StudentRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringDataJpaApplicationTests {
 
+    @Autowired
+    private StudentRepository studentRepository;
+
     @Test
-    void contextLoads() {
+    void selectJPQL() {
+        System.out.println(studentRepository.selectByJPQL());
     }
 
 }
