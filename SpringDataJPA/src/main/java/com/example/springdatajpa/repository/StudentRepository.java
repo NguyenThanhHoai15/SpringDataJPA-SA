9f53cb7c0a5cd6fbe9d8886c9c909df78f894027
NguyenThanhHoai15 <thanhhoai150420@gmail.com>
2021-09-15 09:37:02 +0700

    push code of native branch

--- SpringDataJPA/src/main/java/com/example/springdatajpa/repository/StudentRepository.java
+++ SpringDataJPA/src/main/java/com/example/springdatajpa/repository/StudentRepository.java
@@ -1,7 +1,16 @@
 package com.example.springdatajpa.repository;
 
 import com.example.springdatajpa.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface StudentRepository extends CrudRepository<Student, Long> {
+import java.util.List;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    @Query(value = "Select * from student", nativeQuery = true)
+    List<Student> SelectByNative();
+
+    @Query(value = "delete * from student", nativeQuery = true)
+    void deleteByNative();
 }
